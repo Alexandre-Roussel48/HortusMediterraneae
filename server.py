67a6499575e62ced9e83274b415f43e5d439e7c4
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2023-08-25 23:13:44 +0200

    Server for app runing added

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, redirect, url_for, g, render_template
 from flask_cors import CORS
-#from flask_mail import Mail
 from peewee import MySQLDatabase, Model
 from itsdangerous import (
         TimedSerializer,
@@ -8,16 +7,11 @@ from itsdangerous import (
         BadSignature)
 
 import config
-#from utils.auth import AuthUser
 
-#import utils
-
-#mail = Mail()
 app = Flask('__name__')
-CORS(app)
+#CORS(app)
 
 app.config.from_pyfile('config.py')
-#mail.init_app(app)
 
 db = MySQLDatabase(
         config.db_name, 
@@ -34,49 +28,20 @@ class BaseModel(Model):
 from views import (
     pedagogie,
     medias,
-    especes
-#    base,    
-#    outils,
-#    thesaurus,
-#    pedatheque,
-#    pedatheque_edit,
-#    botanique
+    especes,
+    base
 )
 
-#app.register_blueprint(base.views)
+app.register_blueprint(base.views)
 
 app.register_blueprint(pedagogie.views, url_prefix='/pedagogie')
 app.register_blueprint(medias.views, url_prefix='/medias')
 app.register_blueprint(especes.views, url_prefix='/especes')
-#app.register_blueprint(outils.views, url_prefix='/outils')
-#app.register_blueprint(thesaurus.views, url_prefix='/thesaurus')
-#app.register_blueprint(pedatheque.views, url_prefix='/pedatheque')
-#app.register_blueprint(pedatheque_edit.views, url_prefix='/pedatheque_edit')
-#app.register_blueprint(botanique.views, url_prefix='/base_botanique')
 
 
 @app.before_request
 def _check_user():
-    '''
-    Vérifie que l'utilisateur soit bien authentifié et reconnecte
-    la base de données.
-    '''
-    '''
-    g.user = None
     serializer = TimedSerializer(config.SECRET_KEY)
-
-    try:
-        userdata = serializer.loads(request.cookies.get('user', None))
-        g.user = AuthUser(userdata)
-    except SignatureExpired:
-        return redirect(url_for('main.login_view'))
-    except BadSignature:
-        g.user = AuthUser()
-    except TypeError:
-        g.user = AuthUser()
-    '''
-    serializer = TimedSerializer(config.SECRET_KEY)
-    #g.user = AuthUser()
 
     db.connect(reuse_if_open=True)
 
@@ -92,22 +57,4 @@ def _db_close(exc):
 
 
 if __name__ == '__main__':
-    #from flask_script import Manager
     app.run('0.0.0.0')
-'''
-@app.errorhandler(Exception)
-def page_not_found(e):
-    return render_template('error.htm')
-
-@app.template_filter('md')
-def affichage_md(s):
-    return utils.render_page(s)
-
-@app.template_filter('username_format')
-def user_format(s):
-    return utils.user_format(s)
-
-@app.template_filter('date_format')
-def date_format(s):
-    return s.strftime('%d/%m/%Y')
-'''
